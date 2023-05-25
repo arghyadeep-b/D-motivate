9e8d83087b97e433a6427f3f56b355865a99d909
Arghyadeep Bhowmick <bhowmickarghyadeep2@gmail.com>
2023-05-25 17:19:34 +0530

    Version 1.0.0 ready

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import './App.css';
-import Side from './side';
+import { useState } from 'react';
 
 function App() {
+  const [leftWidth,setLeftWidth]=useState(100);
 
-  const left= document.getElementById("left-side");
+  // const left= document.getElementById("left-side");
 
-  const handleOnMove= e =>{
-    const p= e.clientX / window.innerWidth *100;
+  const handleOnMove = e => {
+    const p = e.clientX / window.innerWidth * 100;
 
-    //! left.style.width= `${p}%`;
-  }
+    setLeftWidth(p);
+  };
 
   return (
-    <div className="app">
-      <div onMouseMove={handleOnMove} id="left-side" className='side'>
-        <Side text="Today is going to be " span="awesome" />
+    <div onMouseMove={handleOnMove}
+    onTouchMove={handleOnMove} className="app">
+      <div style={{width: `${leftWidth}%`}} width={leftWidth} id="left-side" className='side'>
+      <h2 class="title">Today is going to be <span>awesome</span></h2>
       </div>
       <div id="right-side" className='side'>
-        <Side text="Today is going to be " span="wonderful" />
+      <h2 class="title">Today is going to be <span>wonderful</span></h2>
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
